f92a00096a0d970c92d5d144f424ce13575f23fd
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-08-19 12:38:09 +0200

    javascript update

--- src/javascript/button.js
+++ src/javascript/button.js
@@ -13,3 +13,21 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const linksWhatsApp = document.querySelectorAll("a[data-whatsapp]");
+
+  linksWhatsApp.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault(); // previne comportamento padrão do link
+
+      const numero = "5515991271165"; // substitua pelo número real
+      const mensagem = "Olá! Gostaria de mais informações."; // mensagem padrão
+      const url = `https://wa.me/${numero}?text=${encodeURIComponent(
+        mensagem
+      )}`;
+
+      window.open(url, "_blank"); // abre em nova aba / aplicativo
+    });
+  });
+});
